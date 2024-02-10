9e54b775903208329000dec7cb0ba5f7d75e13e3
ezelamar <ezequiel.lamarquee@gmail.com>
2024-02-10 12:33:30 -0300

    SB-25: Add Logout button on NavBar

---
+++ app/firebase/auth/signout.ts
@@ -0,0 +1,15 @@
+import { signOut, getAuth } from 'firebase/auth';
+import firebaseApp from '../config';
+
+const auth = getAuth(firebaseApp);
+
+export default async function logout() {
+  let error = null;
+  try {
+    await signOut(auth);
+  } catch (e) {
+    error = e;
+  }
+
+  return { error };
+}
