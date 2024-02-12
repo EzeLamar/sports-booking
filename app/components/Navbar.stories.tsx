0633e31f5ee37f98785b2ad9447e68ccf53d64cd
ezelamar <ezequiel.lamarquee@gmail.com>
2024-02-11 22:54:43 -0300

    SB-28: Add Storybook and Navbar example

---
+++ app/components/Navbar.stories.tsx
@@ -0,0 +1,67 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { User } from 'firebase/auth';
+import NavBar from './Navbar';
+import { AuthContext } from '../context/AuthContext';
+
+const meta = {
+  title: 'Page/Navbar',
+  component: NavBar,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {},
+
+} satisfies Meta<typeof NavBar>;
+
+export default meta;
+
+const userLogged : User = {
+  email: 'test@gmail.com',
+  emailVerified: false,
+  isAnonymous: false,
+  metadata: undefined,
+  providerData: [],
+  refreshToken: '',
+  tenantId: null,
+  delete: () => {
+    throw new Error('Function not implemented.');
+  },
+  getIdToken: () => {
+    throw new Error('Function not implemented.');
+  },
+  getIdTokenResult: () => {
+    throw new Error('Function not implemented.');
+  },
+  reload: () => {
+    throw new Error('Function not implemented.');
+  },
+  toJSON: () => {
+    throw new Error('Function not implemented.');
+  },
+  displayName: 'Test User',
+  phoneNumber: '0303456',
+  photoURL: null,
+  providerId: '',
+  uid: '',
+};
+
+type Story = StoryObj<typeof meta>;
+
+export const Logged: Story = {
+  decorators: [
+    () => (
+      <AuthContext.Provider value={userLogged}>
+        <NavBar />
+      </AuthContext.Provider>
+    ),
+  ],
+};
+
+export const NotLogged: Story = {
+  decorators: [
+    () => (
+      <AuthContext.Provider value={null}>
+        <NavBar />
+      </AuthContext.Provider>
+    ),
+  ],
+};
