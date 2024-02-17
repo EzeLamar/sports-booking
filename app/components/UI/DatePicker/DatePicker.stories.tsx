985ef9d7f978597ea62b0b871c2813d2a2c865e1
lubualo <lubualo@gmail.com>
2024-02-17 18:45:42 -0300

    SB-33: Added react-day-picker library

---
+++ app/components/UI/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,75 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import { addDays } from 'date-fns';
+import DatePicker from './DatePicker';
+
+const meta = {
+  title: 'UI/DatePicker',
+  component: DatePicker,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: { setSelectedDate: { action: 'clicked' } },
+} satisfies Meta<typeof DatePicker>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const today = new Date();
+const dayAfterTomorrow = addDays(today, 2);
+
+export const Default: Story = {
+  args: {
+  },
+};
+
+export const TestClick: Story = {
+  decorators: [
+    () => {
+      const [selectedDate, setSelectedDate] = useState<Date>(today);
+      return (
+        <DatePicker
+          setSelectedDate={setSelectedDate}
+          selectedDate={selectedDate}
+        />
+      );
+    },
+  ],
+};
+
+export const StartWithADaySelected: Story = {
+  args: {
+    selectedDate: dayAfterTomorrow,
+  },
+};
+
+export const HidePastDays: Story = {
+  args: {
+    hidePastDays: true,
+  },
+};
+
+export const DisableTheDayAfterTomorrow: Story = {
+  args: {
+    daysToDisable: [dayAfterTomorrow],
+  },
+};
+
+export const HidePastDaysAndDisableDayAfterTomorrow: Story = {
+  args: {
+    hidePastDays: true,
+    daysToDisable: [dayAfterTomorrow],
+  },
+};
+
+export const ShowOnlyOneMonthForward: Story = {
+  args: {
+    monthsForwardToBeShown: 1,
+  },
+};
+
+export const ShowOnlyOneMonthBack: Story = {
+  args: {
+    monthsBackToBeShown: 1,
+  },
+};
