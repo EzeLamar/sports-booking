ba29fa4519c29b4ee7c81d90ac0e897300f52a04
ezelamar <ezequiel.lamarquee@gmail.com>
2024-02-22 16:26:10 -0300

    SB-42: Add Multiple Day Selector Component

---
+++ app/components/UI/MultipleOptionsSelector/MultipleOptionsSelector.tsx
@@ -0,0 +1,38 @@
+import MultipleOptionBadge from './MultipleOptionBadge';
+import './MultipleOptionsSelector.css';
+
+type Props = {
+    options: string[],
+    selectedOptions: string[],
+    setSelectedOptions: (selectedOptions: string[]) => void,
+};
+
+export default function MultipleOptionsSelector({
+  options,
+  selectedOptions,
+  setSelectedOptions,
+}: Props) {
+  const handleSelectOption = (option: string) => {
+    const isSelected = selectedOptions.includes(option);
+    const newSelectedOptions = isSelected
+      ? selectedOptions.filter((selected) => selected !== option)
+      : [...selectedOptions, option];
+    setSelectedOptions(newSelectedOptions);
+  };
+
+  return (
+    <div className="multiple-options-selector__container">
+      {options.map(
+        (option, index) => (
+          <MultipleOptionBadge
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            label={option}
+            selected={selectedOptions.includes(option)}
+            select={() => handleSelectOption(option)}
+          />
+        ),
+      )}
+    </div>
+  );
+}
--- app/components/UI/Navbar/Navbar.stories.tsx
+++ app/components/UI/Navbar/Navbar.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { User } from 'firebase/auth';
 import NavBar from './Navbar';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../../../context/AuthContext';
 
 const meta = {
   title: 'Page/Navbar',
