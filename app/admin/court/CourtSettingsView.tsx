ccf6467855221df2819393fba14aa550ae929ac0
ezelamar <ezequiel.lamarquee@gmail.com>
2024-04-22 16:30:56 -0300

    SB-3: Add Vercel Speed Insights for Deploy Metrics

--- app/admin/court/CourtSettingsView.tsx
+++ app/admin/court/CourtSettingsView.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
-import hasErrorMessage from '@/app/utils/Error/ErrorHelper';
+import hasErrorMessage from '../../utils/Error/ErrorHelper';
 import CourtSettings, {
 	Court,
 } from '../../components/Courts/CourtSettings/CourtSettings';
