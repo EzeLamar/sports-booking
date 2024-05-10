6d02f4f8a7bbbfa4c62c2e083083992813e233b3
lubualo <lubualo@gmail.com>
2024-05-10 15:26:40 -0300

    SB-46: Add a create reservation feature

---
+++ app/(admin)/getAllReservations/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import ReservationWidget, {
+	ReservationType,
+} from '../../components/Reservations/Reservation';
+import { getAllReservations } from '../../firebase/reservations/reservation';
+import Loading from '../../components/UI/Loading/Loading';
+import hasErrorMessage from '../../utils/Error/ErrorHelper';
+
+// FIXME: this is just a test view, will be deleted once we have the calendar for reservations
+export default function GetAllReservationsView() {
+	const [loading, setLoading] = useState<boolean>(true);
+	const [reservations, setReservations] = useState<Array<ReservationType>>([]);
+
+	useEffect((): void => {
+		const fetchData = async () => {
+			try {
+				const reservationsData = await getAllReservations();
+				setReservations(reservationsData);
+				setLoading(false);
+			} catch (error: unknown) {
+				if (hasErrorMessage(error)) {
+					toast.error(error.message, { theme: 'colored' });
+				}
+			}
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			{loading ? (
+				<Loading />
+			) : (
+				<>
+					<h2>List of reservations</h2>
+					<ul>
+						{reservations.map(reservation => (
+							<li key={reservation.startTime.toISOString()}>
+								<ReservationWidget reservationInfo={reservation} />
+							</li>
+						))}
+					</ul>
+				</>
+			)}
+		</div>
+	);
+}
--- app/firebase/reservations/reservation.ts
+++ app/firebase/reservations/reservation.ts
@@ -4,12 +4,11 @@ import {
 	getDoc,
 	collection,
 	getDocs,
+	addDoc,
 } from 'firebase/firestore';
 import { ReservationType } from '@/app/components/Reservations/Reservation';
 import moment from 'moment';
 import firebaseApp from '../config';
-
-const reservationId = 'ioPMkgCdwfB8xfsYd9Ze';
 
 function getEndTimeFromStartTimeAndDuration(
 	startTime: Date,
@@ -18,19 +17,27 @@ function getEndTimeFromStartTimeAndDuration(
 	return moment(startTime).add(durationInMin, 'minutes').toDate();
 }
 
-export async function getReservation(): Promise<ReservationType> {
+function getDurationFromStartTimeAndEndTimeInMinutes(
+	startTime: Date,
+	endTime: Date
+): number {
+	return (endTime.getTime() - startTime.getTime()) / 60000;
+}
+
+export async function getReservation(id: string): Promise<ReservationType> {
 	const db = getFirestore(firebaseApp);
 	const docRef = doc(
 		db,
 		process.env.NEXT_PUBLIC_RESERVATIONS_COLLECTION ??
 			'NEXT_PUBLIC_RESERVATIONS_COLLECTION',
-		reservationId
+		id
 	);
 	const docSnap = await getDoc(docRef);
 	if (!docSnap.exists()) {
 		throw new Error('The reservation does not exist.');
 	}
 	const data = { ...docSnap.data() };
+
 	return {
 		court: data.court,
 		owner: data.owner,
@@ -63,4 +70,27 @@ export async function getAllReservations(): Promise<Array<ReservationType>> {
 			),
 		};
 	});
+}
+
+export async function createReservation(
+	reservationData: ReservationType
+): Promise<boolean> {
+	const db = getFirestore(firebaseApp);
+	const colRef = collection(
+		db,
+		process.env.NEXT_PUBLIC_RESERVATIONS_COLLECTION ??
+			'NEXT_PUBLIC_RESERVATIONS_COLLECTION'
+	);
+	const reservation = {
+		court: reservationData.court,
+		owner: reservationData.owner,
+		startTime: reservationData.startTime,
+		duration: getDurationFromStartTimeAndEndTimeInMinutes(
+			reservationData.startTime,
+			reservationData.endTime
+		),
+	};
+	await addDoc(colRef, reservation);
+
+	return true;
 }
