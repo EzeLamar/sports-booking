23d5dbde76f2927a4b0c9f1239ceb9ed41e5d8ba
ezelamar <ezequiel.lamarquee@gmail.com>
2024-05-12 13:52:48 -0300

    SB-61: Move Auth Logic to Layout and fix navigation issues

--- app/components/UI/Loading/Loading.tsx
+++ app/components/UI/Loading/Loading.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Loading() {
 	return (
-		<div className='spinner-border text-primary' role='status'>
-			<span className='visually-hidden'>Loading...</span>
+		<div className='d-flex pt-3 justify-content-center'>
+			<div className='spinner-border text-primary'>
+				<span className='visually-hidden'>Loading...</span>
+			</div>
 		</div>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Home',
+	description: 'Sport-Booking Home App',
+};
+
 export default function Home() {
 	return <main>Home</main>;
 }
